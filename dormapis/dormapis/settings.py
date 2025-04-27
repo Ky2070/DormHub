f1467cc2ab496168ecb4f7b2f0cc9e58f652ec8a
Ky2070 <2051052070Ky@ou.edu.vn>
2025-04-27 22:22:00 +0700

    fix: api users

--- dormapis/dormapis/settings.py
+++ dormapis/dormapis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dorms.apps.DormsConfig',
     'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- dormapis/dorms/views.py
+++ dormapis/dorms/views.py
@@ -12,24 +12,15 @@ from .serializers import UpdateProfileSerializer
 # Create your views here.
 
 
-class UserViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIView):
+class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = serializers.UserSerializer
     parser_classes = [parsers.MultiPartParser, ]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdmin]
 
-    def get_permissions(self):
-        if self.action == 'login':
-            return [AllowAny()]
-        return super().get_permissions()
-
-    @action(methods=['post'], detail=True, url_path='create-student', permission_classes=[IsAuthenticated, IsAdmin])
-    def create_student(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(methods=['get'], url_path='current-user', detail=False, permission_classes=[IsAuthenticated])
+    def get_current_user(self, request):
+        return Response(serializers.UserSerializer(request.user).data)
 
     @action(methods=['put'], detail=True, url_path='update-profile', permission_classes=[IsAuthenticated])
     def update_profile(self, request):
